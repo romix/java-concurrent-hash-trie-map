8e81d3f6fc20d4c52e675a0aee84a0ad034c8c2a
Robert Varga <robert.varga@pantheon.sk>
2014-09-15 09:55:19 +0200

    Add an instantiation micro-benchmark
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>

---
+++ src/test/java/com/romix/scala/collection/concurrent/TestInstantiationSpeed.java
@@ -0,0 +1,39 @@
+package com.romix.scala.collection.concurrent;
+
+import org.junit.Test;
+
+public class TestInstantiationSpeed {
+    private static final int COUNT = 1000000;
+    private static final int ITERATIONS = 10;
+    private static final int WARMUP = 20;
+
+    private long runIteration() {
+        final TrieMap<?, ?>[] maps = new TrieMap<?, ?>[COUNT];
+        final long start = System.nanoTime();
+
+        for (int i = 0; i < COUNT; ++i) {
+            maps[i] = TrieMap.empty();
+        }
+
+        final long stop = System.nanoTime();
+        return stop - start;
+    }
+
+    @Test
+    public void testInstantiation() {
+
+        for (int i = 0; i < WARMUP; ++i) {
+            final long time = runIteration();
+            System.out.println(String.format("Warmup %s took %sns (%sns)", i, time, time / COUNT));
+        }
+
+        long acc = 0;
+        for (int i = 0; i < ITERATIONS; ++i) {
+            final long time = runIteration();
+            System.out.println(String.format("Iteration %s took %sns (%sns)", i, time, time / COUNT));
+            acc += time;
+        }
+
+        System.out.println("Instantiation cost " + acc / ITERATIONS / COUNT + "ns");
+    }
+}
