3e7efd43b645924502c3acc7e5ebe211090e855c
Robert Varga <robert.varga@pantheon.sk>
2016-03-04 19:34:00 +0100

    Hide CNodeBase.csize
    The field is accessed only via methods, so hide it. Also use direct
    access for read (which is equivalent), so Eclipse knows the field is
    actually being used. Also rename updater to UPDATER to show it is a
    constant.
    
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>

--- src/main/java/com/romix/scala/collection/concurrent/CNodeBase.java
+++ src/main/java/com/romix/scala/collection/concurrent/CNodeBase.java
@@ -16,20 +16,21 @@ import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 abstract class CNodeBase<K, V> extends MainNode<K, V> {
     
-    public static final AtomicIntegerFieldUpdater<CNodeBase> updater = AtomicIntegerFieldUpdater.newUpdater(CNodeBase.class, "csize");
+    @SuppressWarnings("rawtypes")
+    private static final AtomicIntegerFieldUpdater<CNodeBase> UPDATER = AtomicIntegerFieldUpdater.newUpdater(CNodeBase.class, "csize");
     
-    public volatile int csize = -1;
+    private volatile int csize = -1;
     
     public boolean CAS_SIZE(int oldval, int nval) {
-    return updater.compareAndSet(this, oldval, nval);
+        return UPDATER.compareAndSet(this, oldval, nval);
     }
     
     public void WRITE_SIZE(int nval) {
-    updater.set(this, nval);
+        UPDATER.set(this, nval);
     }
     
     public int READ_SIZE() {
-    return updater.get(this);
+        return csize;
     }
     
 }
